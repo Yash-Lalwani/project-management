c902c33357cca7f6f5a595873508e6a899ad4e4f
Yash-Lalwani <yash.lalwani10@outlook.com>
2025-09-26 12:47:41 -0400

    .env setup and testing

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./.env",
+});
+
+let myusername = process.env.USERNAME;
+let mypassword = process.env.PASSWORD;
+
+console.log(myusername);
+console.log(mypassword);
+
 console.log("Start of backend code");
