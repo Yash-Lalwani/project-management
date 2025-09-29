5797734f693962ebec49f30aa11f11db0dacd573
Yash-Lalwani <yash.lalwani10@outlook.com>
2025-09-29 13:00:06 -0400

    backend complete

--- src/app.js
+++ src/app.js
@@ -1,27 +1,37 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-// Middlewares basic configurations (do this for every project)
-app.use(express.json({ limit: "16kb" })); // by this code, we can limit the size of the json request by the user, which is 16kb
-app.use(express.urlencoded({ extended: true, limit: "16kb" })); // by this code, we can accept the urlencoded data like space is %20
+// basic configurations
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser());
 
-// CORS configurations
-app.use(cors({
-    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-}));
+// cors configurations
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN?.split(",") || "http://locahost:5173",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    }),
+);
+
+//  import the routes
+
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import projectRouter from "./routes/project.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/projects", projectRouter);
 
 app.get("/", (req, res) => {
-    res.send("Welcome to Togetherly!");
-});
-
-app.get("/about", (req, res) => {
-    res.send("About Page");
+    res.send("Welcome to basecampy");
 });
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 connectDB()
     .then(() => {
         app.listen(port, () => {
-            console.log(`Server is running on port http://localhost:${port}`);
+            console.log(`✅ Server is running on port http://localhost:${port}`);
         });
     })
     .catch((error) => {
